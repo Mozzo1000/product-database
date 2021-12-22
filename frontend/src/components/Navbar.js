b513adb7ba9d49e4199522a102850e98c08f6166
Andreas Backström <andreas@simplymozzo.se>
2021-12-22 17:52:12 +0100

    Add navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,127 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { Tooltip } from '@mui/material';
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    const pages = [
+        {name: 'Products', link: "/products"},
+        {name: 'category', link: "/category"},
+        {name: 'Brand', link: "/brand"},
+    ];
+    const settings = [
+        {name: 'Logout', link: "/logout"},
+    ];
+
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+  
+    const handleOpenNavMenu = (event) => {
+      setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+      setAnchorElUser(event.currentTarget);
+    };
+  
+    const handleCloseNavMenu = () => {
+      setAnchorElNav(null);
+    };
+  
+    const handleCloseUserMenu = () => {
+      setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="x1">
+                <Toolbar disableGutters>
+                    <Typography variant="h6" noWrap component="div" sx={{mr: 2, display: {xs: 'none', md: 'flex'}}}>
+                        product-database
+                    </Typography>
+
+                    <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                        <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu 
+                            id="menu-appbar" 
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom', 
+                                horizontal: 'left',
+                            }} 
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)} 
+                            onClose={handleCloseNavMenu}
+                            sx={{display: {xs: 'block', md: 'none'}}}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page.name} component={Link} to={page.link}>
+                                    <Typography textAlign="center">{page.name}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                    <Typography variant="h6" noWrap component="div" sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                        product-database
+                    </Typography>
+                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                        {pages.map((page) => (
+                            <Button key={page.name} component={Link} to={page.link} sx={{my: 2, color: 'white', display: 'block'}}>
+                                {page.name}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{flexGrow: 0}}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                <Avatar alt="User" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu 
+                            sx={{mt: '45px'}} 
+                            id="menu-appbar" 
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting.name} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting.name}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
+}
+
+export default Navbar
