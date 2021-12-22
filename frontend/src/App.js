1179cd6d3267a54a4ad47b729deae88bf95e1f98
Andreas Backström <andreas@simplymozzo.se>
2021-12-22 17:53:11 +0100

    Add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from './components/Navbar';
+import ProductsPage from './pages/Products'
 
 function App() {
   return (
     <div className="App">
-      <h1>product-database</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route index element={<h1>product-database</h1>} />
+          <Route path="/products" element={<ProductsPage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
