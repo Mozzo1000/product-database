615e2e393231933497d105d8736c9e1277aeb4ef
Andreas Backström <andreas@simplymozzo.se>
2021-12-22 17:58:35 +0100

    Add routes to categories and brands

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import ProductsPage from './pages/Products'
+import CategoriesPage from './pages/Categories'
+import Brandspage from './pages/Brands';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route index element={<h1>product-database</h1>} />
           <Route path="/products" element={<ProductsPage/>} />
+          <Route path="/category" element={<CategoriesPage/>} />
+          <Route path="/brand" element={<Brandspage/>} />
         </Routes>
       </Router>
     </div>
