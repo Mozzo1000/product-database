cd6bb6ece6d3704f38e22675c347d6a8f729df25
Andreas Backström <andreas@simplymozzo.se>
2021-12-22 17:52:23 +0100

    Add auth-header

---
+++ frontend/src/services/auth-header.js
@@ -0,0 +1,10 @@
+//Currently not being used
+export default function authHeader() {
+    const user = JSON.parse(localStorage.getItem('user'));
+  
+    if (user && user.access_token) {
+      return { Authorization: 'Bearer ' + user.access_token };
+    } else {
+      return {};
+    }
+}
