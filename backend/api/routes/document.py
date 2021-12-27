603ce865dd46c782c781ed09eb4f7da9cacb8dc4
Andreas Backström <mozzo242@gmail.com>
2021-12-27 18:48:00 +0100

    Add retrieval of document based on product

--- backend/api/routes/document.py
+++ backend/api/routes/document.py
@@ -1,15 +1,16 @@
 from flask import Blueprint, request, jsonify, current_app
 from werkzeug.utils import secure_filename
 import os
-#from models import Document, DocumentSchema, db
+from models import Document, DocumentSchema, db
+import mimetypes
 
 document_endpoint = Blueprint('document', __name__)
 
-#@document_endpoint.route("/v1/document/<id>")
-#def get_document_by_product(id):
-#    document_schema = DocumentSchema(many=False)
-#    document = Document.query.get(id)
-#    return jsonify(document_schema.dump(document))
+@document_endpoint.route("/v1/document/<id>")
+def get_document_by_product(id):
+    document_schema = DocumentSchema(many=True)
+    document = Document.query.filter_by(product_id=id).all()
+    return jsonify(document_schema.dump(document))
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -22,6 +23,11 @@ def add_document():
             "error": "Bad request",
             "message": "file not given"
         }), 400
+    if not request.form.get("product_id"):
+        return jsonify({
+            "error": "Bad request",
+            "message": "product_id not given"
+        }), 400
 
     file = request.files["file"]
     if file.filename == "":
@@ -32,6 +38,8 @@ def add_document():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(current_app.config["UPLOAD_FOLDER"], filename))
+        new_document = Document(name=filename, type=mimetypes.guess_type(filename)[0], product_id=request.form.get("product_id"))
+        new_document.save_to_db()
         return {
             "message": "Upload complete",
             "name": filename
