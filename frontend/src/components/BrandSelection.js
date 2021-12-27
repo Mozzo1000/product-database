8d48c69bea976f6e56e4703a5131edf47bcd4a54
Andreas Backström <andreas@simplymozzo.se>
2021-12-27 13:13:50 +0100

    Add brand selection component

---
+++ frontend/src/components/BrandSelection.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect }  from 'react'
+import BrandService from "../services/brand.service";
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import { FormControl } from '@mui/material';
+
+function BrandSelection(props) {
+    const [content, setContent] = useState();
+
+    useEffect(() => {
+        BrandService.getAllBrands().then(
+            response => {
+                setContent(response.data);
+            },
+            error => {
+                const resMessage =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+                console.log(resMessage);
+            }
+        )
+      }, []);
+
+    return (
+        <FormControl>
+            <InputLabel id="brand-select-label">Brand</InputLabel>
+            <Select labelId="brand-select-label" label="Brand" value={props.selectedBrand} onChange={e => props.setState(e.target.value)}>
+                {content?.map(item => {
+                    return <MenuItem value={item.id}>{item.name}</MenuItem>
+                })}
+            </Select>
+        </FormControl>
+    )
+}
+
+export default BrandSelection
