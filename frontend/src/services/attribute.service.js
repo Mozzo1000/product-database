7b725b4b02f7263eb18c23ccb099e1440036bed9
Andreas Backström <andreas@simplymozzo.se>
2021-12-27 13:13:16 +0100

    Add attribute service

---
+++ frontend/src/services/attribute.service.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+
+const API_URL = "/v1/";
+
+const getAllAttributes = (filter) => {
+  if(!filter) {
+    return axios.get(API_URL + "attribute", { headers: authHeader() });
+  } else {
+    return axios.get(API_URL + "attribute" + filter, { headers: authHeader() });
+  }
+};
+
+const addAttribute = (product_id, name, value) => {
+  return axios.post(API_URL + "attribute", {
+    product_id,
+    name,
+    value,
+  }, { headers: authHeader() });
+};
+
+
+const getAttribute = (attribute_id) => {
+  return axios.get(API_URL + "attribute/" + attribute_id, {}, { headers: authHeader() });
+};
+
+
+const exportedObject = {
+    getAllAttributes,
+    getAttribute,
+    addAttribute,
+};
+
+export default exportedObject;
