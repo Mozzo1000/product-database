323fdafc8d417be55cb5df602b82b5ecd6e0edb1
Andreas Backström <mozzo242@gmail.com>
2021-12-27 19:11:32 +0100

    Add function to retrieve all documents

--- frontend/src/services/document.service.js
+++ frontend/src/services/document.service.js
@@ -3,6 +3,10 @@ import authHeader from "./auth-header";
 
 const API_URL = "/v1/";
 
+const getAllDocuments = (product_id) => {
+  return axios.get(API_URL + "document/" + product_id,  { headers: authHeader() });
+};
+
 const addDocument = (data) => {
   return axios.post(API_URL + "document", data, {
   }, { headers: authHeader() });
@@ -11,6 +15,7 @@ const addDocument = (data) => {
 
 const exportedObject = {
     addDocument,
+    getAllDocuments,
 };
 
 export default exportedObject;
