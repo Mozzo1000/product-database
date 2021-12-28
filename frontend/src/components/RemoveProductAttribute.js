4a014bb91c34f08013839cb34f2f44fce9e8508e
Andreas Backström <andreas@simplymozzo.se>
2021-12-28 13:12:16 +0100

    Add remove attribute component

---
+++ frontend/src/components/RemoveProductAttribute.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect }  from 'react'
+import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import FormControl from '@mui/material/FormControl';
+import AttributeService from "../services/attribute.service";
+import Snackbar from '@mui/material/Snackbar';
+
+function RemoveProductAttribute(props) {
+    const [openModal, setOpenModal] = useState(false);
+    const [openStatusMessage, setOpenStatusMessage] = useState(false);
+    const [statusMessage, setStatusMessage] = useState("");
+
+    const handleClickOpenModal = () => {
+        setOpenModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    };
+
+    const handleCloseMessage = () => {
+        setOpenStatusMessage(false);
+    };
+
+    const handleRemoveAttribute = (e) => {
+        e.preventDefault();
+        AttributeService.removeAttribute(props.props.id).then(
+            response => {
+                setOpenModal(false);
+                setStatusMessage(response.data.message + ". Please refresh the page");
+                setOpenStatusMessage(true);
+            },
+            error => {
+                const resMessage =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+                setStatusMessage(resMessage);
+                setOpenStatusMessage(true);
+            }
+        )
+    }
+
+    return (
+        <div>
+            <Button size="small" onClick={handleClickOpenModal}><DeleteIcon /></Button>
+            <Dialog open={openModal} onClose={handleCloseModal}>
+                <DialogTitle>Are you sure?</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                            Removing attribute, <strong>{props.props.name}</strong> CANNOT be undone!
+                    </DialogContentText>
+                </DialogContent>
+                <FormControl>
+                    <form onSubmit={handleRemoveAttribute}>
+                        <DialogActions>
+                            <Button onClick={handleCloseModal}>Cancel</Button>
+                            <Button type="submit" color="error" onClick={handleCloseModal}>Yes</Button>
+                        </DialogActions>
+                    </form>
+                </FormControl>
+            </Dialog>
+            <Snackbar open={openStatusMessage} autoHideDuration={6000} onClose={handleCloseMessage} message={statusMessage} />
+        </div>
+    )
+}
+
+export default RemoveProductAttribute
