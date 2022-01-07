cf8a5fab4274145e956a2f70efaef314bcddfdae
Andreas Backström <mozzo242@gmail.com>
2022-01-07 15:01:55 +0100

    Add brand endpoint documentation

--- backend/api/routes/brand.py
+++ backend/api/routes/brand.py
@@ -5,18 +5,59 @@ brand_endpoint = Blueprint('brand', __name__)
 
 @brand_endpoint.route("/v1/brands/<id>")
 def get_brand(id):
+    """
+        Get brand by id
+        ---
+        tags:
+          - brands
+        parameters:
+          - name: id
+            in: path
+            description: ID of brand
+            required: true
+            type: integer
+        responses:
+          200:
+            description: Returns information about the specific brand, empty if none exists.
+    """
     brand_schema = BrandSchema(many=False)
     brand = Brand.query.get(id)
     return jsonify(brand_schema.dump(brand))
 
 @brand_endpoint.route("/v1/brands")
 def get_brands():
+    """
+        Get all brands
+        ---
+        tags:
+          - brands
+        responses:
+          200:
+            description: Returns a list of all brands, empty if none exists.        
+    """
     brand_schema = BrandSchema(many=True)
     brands = Brand.query.all()
     return jsonify(brand_schema.dump(brands))
 
 @brand_endpoint.route("/v1/brands", methods=["POST"])
 def add_brand():
+    """
+        Add brand
+        ---
+        tags:
+          - brands
+        parameters:
+          - name: name
+            in: body
+            description: Name of brand
+            required: true
+            type: string
+        responses:
+          201:
+            description: Brand added
+          500:
+            description: Unknown error occurred
+    """
     if not "name" in request.json:
         return jsonify({
             "error": "Bad request",
@@ -35,6 +76,23 @@ def add_brand():
 
 @brand_endpoint.route('/v1/brands/<id>', methods=["DELETE"])
 def remove_brand(id):
+    """
+        Remove brand
+        ---
+        tags:
+          - brands
+        parameters:
+          - name: id
+            in: path
+            description: ID of brand
+            required: true
+            type: integer
+        responses:
+          200:
+            description: Brand removed
+          500:
+            description: Unknown error occurred
+    """
     brand = Brand.query.get(id)
     try: 
         db.session.delete(brand)
