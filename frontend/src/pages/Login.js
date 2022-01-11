69c0b182be28e769ac432d6e23bdebab78afbbdf
Andreas Backström <andreas@simplymozzo.se>
2022-01-11 17:00:51 +0100

    Add particles as a background

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -9,7 +9,7 @@ import Button from '@mui/material/Button';
 import AuthService from "../services/auth.service";
 import Snackbar from '@mui/material/Snackbar';
 import { useNavigate } from 'react-router-dom';
-
+import Particles from "react-tsparticles";
 
 function LoginPage(props) {
     const [username, setUsername] = useState("");
@@ -44,6 +44,82 @@ function LoginPage(props) {
 
     return (
         <Container>
+            <Particles
+                id="tsparticles"
+                options={{
+                    background: {
+                        color: {
+                            value: "#1976d2",
+                        },
+                    },
+                    fpsLimit: 60,
+                    interactivity: {
+                    events: {
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        bubble: {
+                            distance: 400,
+                            duration: 2,
+                            opacity: 0.8,
+                            size: 40,
+                        },
+                        push: {
+                        quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                    },
+                    particles: {
+                        color: {
+                            value: "#ffffff",
+                        },
+                        links: {
+                            color: "#ffffff",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        collisions: {
+                            enable: true,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outMode: "bounce",
+                            random: false,
+                            speed: 0.2,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                            enable: true,
+                            area: 800,
+                            },
+                            value: 80,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            random: true,
+                            value: 5,
+                        },
+                    },
+                    detectRetina: true,
+                }}
+                />
             <Grid container spacing={0} justifyContent="center" direction="row">
                 <Grid item>
                     <Grid container direction="column" justifyContent="center" spacing={2} className="login-form">
