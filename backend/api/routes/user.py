c7790269a0e1c7ba7562b6c7f9fa1314ed973493
Andreas Backström <andreas@simplymozzo.se>
2022-01-20 16:46:55 +0100

    Add user image upload and serving

--- backend/api/routes/user.py
+++ backend/api/routes/user.py
@@ -1,9 +1,17 @@
-from flask import Blueprint, request, jsonify, abort
+from flask import Blueprint, request, jsonify, abort, send_from_directory, current_app
+from werkzeug.utils import secure_filename
 from flask_jwt_extended import (create_access_token, create_refresh_token,
                                 jwt_required, get_jwt_identity, get_jwt)
 from models import User, UserSchema
+import os
 
 user_endpoint = Blueprint('user', __name__)
+
+@user_endpoint.route('/v1/users/storage/<path:filename>', methods=['GET'])
+def get_image(filename):
+    directory = os.path.join(os.getcwd(), current_app.config['UPLOAD_FOLDER_PROFILE'])
+    return send_from_directory(directory, filename, as_attachment=False)
+
 
 @user_endpoint.route("/v1/users/me", methods=["GET"])
 @jwt_required()
@@ -15,19 +23,32 @@ def get_logged_in_user():
 @user_endpoint.route("/v1/users/me", methods=["PATCH"])
 @jwt_required()
 def edit_logged_in_user():
-    if not request.json:
+    if not request.json and not request.files:
         return jsonify({
             "error": "Bad request",
-            "message": "name, email and/or password not given"
-        }), 400
+            "message": "name, email, password and/or image not given"
+       }), 400
+    
     user = User.query.filter_by(email=get_jwt_identity()).first()
-    print(request.json)
-    if "name" in request.json:
-        user.name = request.json["name"]
-    if "email" in request.json:
-        user.email = request.json["email"]
-    if "password" in request.json:
-        user.password = User.generate_hash(request.json["password"])
+
+    if request.files:
+        if "image" in  request.files:
+            image = request.files["image"]
+            if image.filename == "":
+                return jsonify({
+                    "error": "Bad request",
+                    "message": "image is empty"
+                }), 400
+            image_filename = secure_filename(image.filename)
+            image.save(os.path.join(current_app.config["UPLOAD_FOLDER_PROFILE"], image_filename))
+            user.image = image_filename
+    else:
+        if "name" in request.json:
+            user.name = request.json["name"]
+        if "email" in request.json:
+            user.email = request.json["email"]
+        if "password" in request.json:
+            user.password = User.generate_hash(request.json["password"])
     user.save_to_db()
     return jsonify({'message': 'User settings saved'}), 200
 
