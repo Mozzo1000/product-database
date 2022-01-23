c7a71e2e2285a5cb9284a2736576e9c7d913aae2
Andreas Backström <mozzo242@gmail.com>
2022-01-23 18:14:47 +0100

    Add get all users service

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -4,7 +4,6 @@ import authHeader from "./auth-header";
 const API_URL = "/v1/";
 
 const getMe = () => {
-  console.log(authHeader());
   return axios.get(API_URL + "users/me", { headers: authHeader() });
 };
 
@@ -12,9 +11,14 @@ const editMe = (data) => {
   return axios.patch(API_URL + "users/me", data, { headers: authHeader() });
 };
 
+const getAllUsers = () => {
+  return axios.get(API_URL + "users", {headers: authHeader() });
+}
+
 const exportedObject = {
     getMe,
     editMe,
+    getAllUsers,
 };
 
 export default exportedObject;
