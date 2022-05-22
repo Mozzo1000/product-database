4e0de1a39c94fcaa643e38da735f28f9715f6574
Andreas Backström <andreas@simplymozzo.se>
2022-05-22 12:38:23 +0200

    Change background color

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#f8fbfb"
+    }
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
