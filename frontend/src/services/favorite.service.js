f0444db6300f6e7a8ac95bb0a78cd2d29eead79e
Andreas Backström <mozzo242@gmail.com>
2022-05-28 16:45:55 +0200

    Add service for favorite endpoint

---
+++ frontend/src/services/favorite.service.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+
+const API_URL = "/v1/";
+
+const add = (product_id) => {
+  return axios.post(API_URL + "favorites", {product_id}, { headers: authHeader() });
+};
+
+const getAll = () => {
+  return axios.get(API_URL + "favorites", {}, { headers: authHeader() });
+};
+
+const remove = (product_id) => {
+    return axios.delete(API_URL + "favorites/" + product_id, { headers: authHeader() });
+};
+
+const isFavorite = (product_id) => {
+    return axios.get(API_URL + "favorites/" + product_id, { headers: authHeader() });
+};
+
+const exportedObject = {
+    add,
+    getAll,
+    remove,
+    isFavorite,
+};
+
+export default exportedObject;
