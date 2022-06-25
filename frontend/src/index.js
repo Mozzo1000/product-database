f35b2a70be82c75761166c1e1b1b05d7673a3375
Andreas Backström <mozzo242@gmail.com>
2022-06-25 19:52:24 +0200

    Add i18n

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './i18n';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
 import { SnackbarProvider } from 'material-ui-snackbar-provider';
 import Alert from './components/Alerts/Alert';
-
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme({
